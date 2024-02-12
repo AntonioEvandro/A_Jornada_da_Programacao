12720254e55da824133c8e586b2e354f380be3b2
Miqueiasxz <miqueiasalencar2002@gmail.com>
2024-02-12 11:18:08 -0300

    Update

--- Projeto/Assets/Menu Principal/ControlePersonagem.cs
+++ Projeto/Assets/Menu Principal/ControlePersonagem.cs
@@ -6,14 +6,18 @@ public class ControlePersonagem : MonoBehaviour
 {
     public float velocidade = 5f; 
     private Rigidbody2D rb; 
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 
+        // Adiciona um BoxCollider2D ao objeto do jogador
         BoxCollider2D boxCollider = gameObject.AddComponent<BoxCollider2D>();
+        boxCollider.size = new Vector2(1f, 1f);
 
-        boxCollider.size = new Vector2();
+        // Obtém a referência ao Animator
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -24,19 +28,39 @@ public class ControlePersonagem : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             movimentoVertical = 1f;
+
+            // Ativa a animação "Walking Up" no Animator
+            animator.SetBool("Walking Up", true);
         }
         else if (Input.GetKey(KeyCode.S))
         {
             movimentoVertical = -1f;
+
+            // Ativa a animação "Walking Down" no Animator
+            animator.SetBool("Walking Down", true);
+        }
+        else
+        {
+            // Desativa as animações verticais se as teclas não estiverem pressionadas
+            animator.SetBool("Walking Up", false);
+            animator.SetBool("Walking Down", false);
+            animator.SetBool("Walking Left", false);
+
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             movimentoHorizontal = -1f;
+
+            // Ativa a animação "Walking Left" no Animator
+            animator.SetBool("Walking Left", true);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             movimentoHorizontal = 1f;
+
+            // Desativa a animação "Walking Left" se a tecla não estiver pressionada
+            animator.SetBool("Walking Left", false);
         }
 
         Vector2 movimento = new Vector2(movimentoHorizontal, movimentoVertical);
@@ -51,6 +75,7 @@ public class ControlePersonagem : MonoBehaviour
 
     void FixedUpdate()
     {
+        // Evitar atravessar objetos no cenário
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.2f);
         foreach (Collider2D collider in colliders)
         {
