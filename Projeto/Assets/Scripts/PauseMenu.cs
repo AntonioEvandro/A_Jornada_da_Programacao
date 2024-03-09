dae1034415e3c4aefbb023f54549317aed3d8aaf
AntonioEvandro <evandroalves123321@gmail.com>
2024-03-09 20:13:13 -0300

    screens repaired

--- Projeto/Assets/Scripts/PauseMenu.cs
+++ Projeto/Assets/Scripts/PauseMenu.cs
@@ -8,8 +8,10 @@ public class PauseMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (!pauseMenu.gameObject.activeSelf)
+        if (pauseMenu.gameObject.activeSelf)
         {
+            Time.timeScale = 0;
+        }else{
             Time.timeScale = 1;
         }
     }
