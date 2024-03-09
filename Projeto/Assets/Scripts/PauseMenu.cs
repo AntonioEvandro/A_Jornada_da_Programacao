a04b0c14319ec8175b66421f308bc3a851e2f72c
AntonioEvandro <evandroalves123321@gmail.com>
2024-03-09 12:59:50 -0300

    Pause menu fixed

--- Projeto/Assets/Scripts/PauseMenu.cs
+++ Projeto/Assets/Scripts/PauseMenu.cs
@@ -8,7 +8,10 @@ public class PauseMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!pauseMenu.gameObject.activeSelf)
+        {
+            Time.timeScale = 1;
+        }
     }
 
     // Update is called once per frame
