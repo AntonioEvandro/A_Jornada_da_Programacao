4b924e924ab289d0bdc1bf9e4736ddba1a524d02
AntonioEvandro <evandroalves123321@gmail.com>
2024-05-08 12:49:08 -0300

    SaveGame updated

--- Projeto/Assets/Scripts/Save.cs
+++ Projeto/Assets/Scripts/Save.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Save : MonoBehaviour
+public class Save
 {
     public int life;
     public int score;
--- Projeto/Assets/Scripts/SaveLoad.cs
+++ Projeto/Assets/Scripts/SaveLoad.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using TreeEditor;
 using UnityEngine;
 
 public class SaveLoad : MonoBehaviour
@@ -11,6 +12,7 @@ public class SaveLoad : MonoBehaviour
     {
         Save s = new Save();
 
+        s.life = 3;
         s.score = 0;
         s.aid = 0;
         s.tips = 0;
@@ -18,20 +20,38 @@ public class SaveLoad : MonoBehaviour
         s.partner = false;
         s.missions = new List<bool>
         {
-            false, false, false, false, false, false, false, false
+            false, false, false, false, false, false, false, false, false
         };
 
-        SaveGame(s);
+        //SaveGame(s);
 
         Save load = LoadGame();
 
-        Debug.Log(load.missions[0]);
+        Debug.Log(
+            "<color=red> Vida: </color>" + load.life + 
+            "<color=yellow> Pontos: </color>" +  load.score + 
+            "<color=blue> Ajudas: </color>" +  load.aid + 
+            "<color=green> Dicas: </color>" + load.tips + 
+            "<color=orange> -2: </color>" + load.m2 + 
+            "<color=cyan> Companheiro: </color>" + s.partner
+        );
+        Debug.Log(
+            "<color=black> Missões: </color>" +
+            "<color=with> 0: </color>" + s.missions[0] + 
+            "<color=with> 1: </color>" + s.missions[1] + 
+            "<color=with> 2: </color>" + s.missions[2] + 
+            "<color=with> 3: </color>" + s.missions[3] + 
+            "<color=with> 4: </color>" + s.missions[4] + 
+            "<color=with> 5: </color>" + s.missions[5] + 
+            "<color=with> 6: </color>" + s.missions[6] + 
+            "<color=with> 7: </color>" + s.missions[7] + 
+            "<color=with> 8: </color>" + s.missions[8]);
     }
 
     public void SaveGame(Save s){
         BinaryFormatter bf = new BinaryFormatter();
 
-        string path = Application.persistentDataPath; //AppData/LocalLow/SeuNome
+        string path = Application.persistentDataPath; //C:\Users\T-Gamer\AppData\LocalLow\DefaultCompany\Projeto
         FileStream file = File.Create(path + "/savegame.save");
 
         bf.Serialize(file, s);
