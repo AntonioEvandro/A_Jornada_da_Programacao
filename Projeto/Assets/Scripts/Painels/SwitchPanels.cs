85ed9f8904bf524b3401054a093d3d4a87ecf375
AntonioEvandro <evandroalves123321@gmail.com>
2024-05-26 20:39:42 -0300

    Update SwitchPanels.cs

--- Projeto/Assets/Scripts/Painels/SwitchPanels.cs
+++ Projeto/Assets/Scripts/Painels/SwitchPanels.cs
@@ -27,20 +27,46 @@ public class SwitchPanels : MonoBehaviour
             PauseMenuOn();
         }else if(Input.GetKeyDown(KeyCode.Escape) && !market.transform.gameObject.activeSelf && isGamePaused){
             PauseMenuOff();
+        }else if(Input.GetKey(KeyCode.Escape) && market.transform.gameObject.activeSelf){
+            MarketOff();
+        }
+        //esses if's de mercado abaixo é apenas para desenvolvimento, apague/comente depois que der certo
+        if (Input.GetKeyDown(KeyCode.M) && !pauseMenu.transform.gameObject.activeSelf && !isGamePaused){
+            MarketOn();
+        }else if(Input.GetKeyDown(KeyCode.M) && !pauseMenu.transform.gameObject.activeSelf && isGamePaused){
+            MarketOff();
         }
         
     }
+    //Funções para mostrar/ocultar tela de pausa
     public void PauseMenuOn(){
         pauseMenu.SetActive(true);
         TogglePauseState(true);
         quests.transform.gameObject.SetActive(false);
         dialogueBox.transform.gameObject.SetActive(false);
+        panelUI.SetActive(false);
+        HUD.enabled = false;
     }
     public void PauseMenuOff(){
         pauseMenu.SetActive(false);
         TogglePauseState(false);
         quests.transform.gameObject.SetActive(true);
         dialogueBox.transform.gameObject.SetActive(true);
+        panelUI.SetActive(true);
+        HUD.enabled = true;
+    }
+    // Funçoes para mostrar/ocultar tela do mercado
+    public void MarketOn(){
+        market.SetActive(true);
+        TogglePauseState(true);
+        panelUI.SetActive(false);
+        HUD.enabled = false;
+    }
+    public void MarketOff(){
+        market.SetActive(false);
+        TogglePauseState(false);
+        panelUI.SetActive(true);
+        HUD.enabled = true;
     }
     private void TogglePauseState(bool pause)
     {
