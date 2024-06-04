b147ec635de9efe7a5c9f090d06caab034984ed6
AntonioEvandro <evandroalves123321@gmail.com>
2024-06-04 16:33:08 -0300

    Partner

--- Projeto/Assets/Scripts/Save/Partner.cs
+++ Projeto/Assets/Scripts/Save/Partner.cs
@@ -14,6 +14,16 @@ public class Partner : MonoBehaviour
     [Range(0.01f, 10f)]
     [SerializeField]
     private float speed;
+
+    [SerializeField]
+    private float minimalDistance;
+
+    [Tooltip("RigidBody do companheiro")]
+    [SerializeField]
+    private Rigidbody2D rb;
+    [SerializeField]
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,12 +36,25 @@ public class Partner : MonoBehaviour
         FollowPlayer();
     }
     private void FollowPlayer(){
-        if(playerPose.gameObject != null){
+        if(playerPose.gameObject != null && player.GetComponent<Items>().LoadPartner()){
             //transform.position = Vector2.MoveTowards(transform.position, playerPose.position, speed * Time.deltaTime);
             Vector2 playerPos = this.playerPose.position;
             Vector2 currentPos = this.transform.position;
-            Vector2 direction = playerPos - currentPos;
-            Debug.Log("Direção" + direction);
+
+            float distance = Vector2.Distance(playerPos, currentPos);
+            if (distance >= this.minimalDistance){
+                Vector2 direction = playerPos - currentPos;
+                direction = direction.normalized;
+                //Debug.Log("Direção" + direction);
+                this.rb.velocity = (this.speed * direction);
+                //Debug.Log("velocidade" + this.rb.velocity);
+                animator.SetFloat("Horizontal", direction.x);
+                animator.SetFloat("Vertical", direction.y);
+            }else{
+                this.rb.velocity = Vector2.zero; // (0,0)
+                animator.SetFloat("Horizontal", 0);
+                animator.SetFloat("Vertical", -1);
+            }
         }
     }
 }
