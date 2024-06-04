f81a82f4af4c9a9f67f0850cf2247dd114337c43
AntonioEvandro <evandroalves123321@gmail.com>
2024-06-04 15:51:35 -0300

    Partner
    Sistema de companheiro

--- Projeto/Assets/Scripts/Save/Partner.cs
+++ Projeto/Assets/Scripts/Save/Partner.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Partner : MonoBehaviour
 {
+    [Tooltip("Componente do Jogador")]
+    [SerializeField]
+    private GameObject player;
+    private Transform playerPose;
+
+    [Header("Speed Partner")]
+    [Tooltip("Velocidade do companheiro")]
+    [Range(0.01f, 10f)]
+    [SerializeField]
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerPose = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FollowPlayer();
+    }
+    private void FollowPlayer(){
+        if(playerPose.gameObject != null){
+            transform.position = Vector2.MoveTowards(transform.position, playerPose.position, speed * Time.deltaTime);
+        }
     }
 }
