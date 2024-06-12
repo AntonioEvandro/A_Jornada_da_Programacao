0ab7f32d6d19fa8b8e6c1ce85c1dd6177ceaa2a5
AntonioEvandro <evandroalves123321@gmail.com>
2024-06-12 17:09:06 -0300

    DialogState fixed

--- Projeto/Assets/Scripts/SaveSystem/Dialogs.cs
+++ Projeto/Assets/Scripts/SaveSystem/Dialogs.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
 public enum DialogState{
     Exibir, Exibindo, Exibido
 }
+[System.Serializable]
 public class Dialogs
 {
     public DialogState type;
